67f66b5d73a929fc8bc643339cc56470308074fe
lovethukeachusim <ukeloveth247@gmail.com>
2025-08-17 02:05:23 +0100

    added push notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -47,25 +47,25 @@
 
 
 // IMPORTANT: use a compat CDN version that matches your SDK major version
-importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-messaging-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-messaging-compat.js');
 
-    firebase.initializeApp({
-        apiKey: "AIzaSyC8QS2khALh2u4SQt2JB_QEcz1GBuLHxjo",
-        authDomain: "qrcode-b2f82.firebaseapp.com",
-        projectId: "qrcode-b2f82",
-        storageBucket: "qrcode-b2f82.firebasestorage.app",
-        messagingSenderId: "721686140912",
-        appId: "1:721686140912:web:5be91d8c070b558f58ef35",
-        measurementId: "G-0L2J7BEJJ0"
-    });
+//     firebase.initializeApp({
+//         apiKey: "AIzaSyC8QS2khALh2u4SQt2JB_QEcz1GBuLHxjo",
+//         authDomain: "qrcode-b2f82.firebaseapp.com",
+//         projectId: "qrcode-b2f82",
+//         storageBucket: "qrcode-b2f82.firebasestorage.app",
+//         messagingSenderId: "721686140912",
+//         appId: "1:721686140912:web:5be91d8c070b558f58ef35",
+//         measurementId: "G-0L2J7BEJJ0"
+//     });
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
-// handle background messages
-messaging.onBackgroundMessage(function(payload) {
-    console.log(payload,"New notofication------")
-  const title = payload.notification?.title || 'Background message';
-  const options = { body: payload.notification?.body || '', icon: '/favicon.ico' };
-  self.registration.showNotification(title, options);
-});
+// // handle background messages
+// messaging.onBackgroundMessage(function(payload) {
+//     console.log(payload,"New notofication------")
+//   const title = payload.notification?.title || 'Background message';
+//   const options = { body: payload.notification?.body || '', icon: '/favicon.ico' };
+//   self.registration.showNotification(title, options);
+// });
